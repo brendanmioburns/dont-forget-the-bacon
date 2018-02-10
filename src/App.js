954f8e1988b38b8818864fe03b1dce230248a0a7
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 12:42:09 -0800

    update App.js, create App.js in components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
-import './App.css';
+import './index.css';
+import App from './components/App';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
