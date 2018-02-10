1cc0c89a546c616cd9c8e364a7ad667ba176c1c4
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 14:50:02 -0800

    destructure store from props, add state object, use store.subscribe to set component's state as the latest state from store

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import { Component } from 'react';
 
 class App extends Component {
+  state = {
+    calendar: null
+  }
+  componentDidMount() {
+    const { store } = this.props;
+
+    store.subscribe(() => {
+      this.setState(() => {
+        calendar: store.getState()
+      })
+    })
+  }
   render() {
     return (
       'Hello World'
