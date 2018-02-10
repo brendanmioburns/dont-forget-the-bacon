760224575769dab43881de7ed91e5af75f4729c4
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 12:31:27 -0800

    install redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+import './index.css'
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Don't Forget The Bacon</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
