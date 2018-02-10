f9a1fb9642b73e2091584f2939df1b6333e46309
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 13:44:30 -0800

    add redux devtools extension to createStore parameters

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
-
-console.log(store.getState());
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
