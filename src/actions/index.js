1c0a1ab5af103477eca636346ca3f3660b4aab91
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 11:45:57 -0800

    add constants to avoid having string comparisons everywhere

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
+
 {
-  type: 'ADD_RECIPE',
+  type: ADD_RECIPE,
   recipe,
   day,
   meal,
 }
 
 {
-  type: 'REMOVE_FROM_CALENDAR',
+  type: REMOVE_FROM_CALENDAR,
   recipe,
   day,
   meal,
