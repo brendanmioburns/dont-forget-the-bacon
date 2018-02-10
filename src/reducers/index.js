b91283fb58f7fe84273267705d49216282bc4c58
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 12:17:29 -0800

    specify state returned based on action type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -47,15 +47,23 @@ function calendar (state = initialCalendarState, action) {
   switch (action.type) {
     case ADD_RECIPE :
       return {
-
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label,
+        }
       }
     case REMOVE_FROM_CALENDAR :
       return {
-
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null,
+        }
       }
     default :
       return {
-        
+        state
       }
   }
 }
