00578095f3acb080c1ae613cd87acc01c5fac113
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-10 13:38:22 -0800

    create store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -67,3 +67,5 @@ function calendar (state = initialCalendarState, action) {
       }
   }
 }
+
+export default calendar;
