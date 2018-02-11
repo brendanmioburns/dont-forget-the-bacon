14c8ff3e1243a96b297b0553314fccf98a07a40d
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-11 11:51:43 -0800

    create mapDispatchToProps to bind dispatch() to action creators before they hit component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
   render() {
+    console.log('Props', this.props)
     return (
       <div>
         Hello world
@@ -22,4 +24,11 @@ function mapStateToProps(calendarStateFromStore) {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
