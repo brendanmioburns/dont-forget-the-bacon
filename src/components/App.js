9e10559be828201fa00d207c222b1ff5b89b6656
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-11 17:09:52 -0800

    update mapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -13,14 +13,20 @@ class App extends Component {
   }
 }
 
-function mapStateToProps(calendarStateFromStore) {
-  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+function mapStateToProps ({ food, calendar }) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
   return {
     calendar: dayOrder.map((day) => ({
       day,
-      meals: Object.assign({}, calendarStateFromStore[day])
-    }))
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? food[calendar[day][meal]]
+          : null
+
+        return meals
+      }, {})
+    })),
   }
 }
 
