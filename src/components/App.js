905e2c9ca0e774d929cb152e4c28c4e517781c93
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-16 19:42:18 -0800

    create top grid row, add css

--- src/components/App.js
+++ src/components/App.js
@@ -6,37 +6,46 @@ import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
 
 class App extends Component {
   render() {
-    console.log('Props', this.props)
+    const { calendar, remove } = this.props
+    const mealOrder = ['breakfast', 'lunch', 'dinner']
+
     return (
-      <div>
-        Hello world
+      <div className='container'>
+        <ul className='meal-types'>
+          {mealOrder.map((mealType) => (
+            <li key={mealType} className='subheader'>
+              {capitalize(mealType)}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ food, calendar }) {
-  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-
-  return {
-    calendar: dayOrder.map((day) => ({
-      day,
-      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-        meals[meal] = calendar[day][meal]
-          ? food[calendar[day][meal]]
-          : null
-
-        return meals
-      }, {})
-    })),
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    selectRecipe: (data) => dispatch(addRecipe(data)),
-    remove: (data) => dispatch(removeFromCalendar(data)),
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// function mapStateToProps({ food, calendar }) {
+//   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+//
+//   return {
+//     calendar: dayOrder.map((day) => ({
+//       day,
+//       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+//         meals[meal] = calendar[day][meal]
+//           ? food[calendar[day][meal]]
+//           : null
+//
+//         return meals
+//       }, {})
+//     })),
+//   }
+// }
+//
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     selectRecipe: (data) => dispatch(addRecipe(data)),
+//     remove: (data) => dispatch(removeFromCalendar(data)),
+//   }
+// }
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
