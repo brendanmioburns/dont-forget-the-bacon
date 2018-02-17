513dd262687519a91a41fe60111df6a83c53eb9f
Brendan Michael Iwao Ozawa Burns <brendanmioburns@gmail.com>
2018-02-16 20:05:57 -0800

    add day subheader mapping to grid

--- src/components/App.js
+++ src/components/App.js
@@ -18,34 +18,39 @@ class App extends Component {
             </li>
           ))}
         </ul>
+
+        <div className="calendar">
+          <div className="days">
+            {calendar.map(({ day }) => <h3 key={day} className="subheader">{capitalize(day)}</h3>)}
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-// function mapStateToProps({ food, calendar }) {
-//   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-//
-//   return {
-//     calendar: dayOrder.map((day) => ({
-//       day,
-//       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-//         meals[meal] = calendar[day][meal]
-//           ? food[calendar[day][meal]]
-//           : null
-//
-//         return meals
-//       }, {})
-//     })),
-//   }
-// }
-//
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     selectRecipe: (data) => dispatch(addRecipe(data)),
-//     remove: (data) => dispatch(removeFromCalendar(data)),
-//   }
-// }
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
-export default App;
+function mapStateToProps({ food, calendar }) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? food[calendar[day][meal]]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
